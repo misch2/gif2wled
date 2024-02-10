2518f1b7df6a1d1bb1c281d08c314e7b680dcfb7
Michal Schwarz <michal.schwarz@gmail.com>
2024-02-10 19:58:11 +0100

    wip: more generic

--- server.js
+++ server.js
@@ -1,33 +1,42 @@
 const http = require("http");
+const AnimatedGif2E131 = require("animatedgif2e131");
+const fs = require("fs");
 
-const host = "localhost";
-const port = 8000;
+const listenhost = "localhost";
+const listenport = 8000;
 
-var AnimatedGif2E131 = require("animatedgif2e131");
+var currentGif;
+var currentTimeout;
 
-var fs = require("fs");
+function playGifForSpecifiedTime(duration, filename, fps = 25) {
+    var options = {
+        port: 5568,
+        host: "10.52.6.54",
+    };
 
-var options = {
-    port: 5568,
-    host: "10.52.6.54",
-};
+    console.log("Loading gif from " + filename);
+    var buf = fs.readFileSync(filename);
+    var mappingFunction = AnimatedGif2E131.mapping.snake;
 
-var buf = fs.readFileSync("fire16x16.gif");
+    if (currentTimeout) {
+        console.log("Clearing previous timeout");
+        clearTimeout(currentTimeout);
+    }
 
-var mappingFunction = AnimatedGif2E131.mapping.snake;
+    if (currentGif) {
+        console.log("Stopping previous gif animation.");
+        currentGif.stopAnimation();
+        currentGif.output.close(() => {}); // disconnect from the E131 server explicitly
+    }
+    currentGif = new AnimatedGif2E131(buf, options, mappingFunction);
+    currentGif.startAnimation(fps);
 
-var theGif = new AnimatedGif2E131(buf, options, mappingFunction);
-
-var fps = 25;
-
-theGif.startAnimation(fps);
-// sleep 1 second
-setTimeout(() => {
-    theGif.stopAnimation();
-    theGif.output.close(() => {
-        console.log("done");
-    })
-}, 1000);
+    currentTimeout = setTimeout(() => {
+        currentGif.stopAnimation();
+        currentGif.output.close(() => {}); // disconnect from the E131 server explicitly
+    }, duration);
+    console.log("Playing gif for " + duration + "ms");
+}
 
 const requestListener = function (req, res) {
     res.writeHead(200);
@@ -35,6 +44,11 @@ const requestListener = function (req, res) {
 };
 
 const server = http.createServer(requestListener);
-server.listen(port, host, () => {
-    console.log(`Server is running on http://${host}:${port}`);
+server.listen(listenport, listenhost, () => {
+    console.log(`Server is running on http://${listenhost}:${listenport}`);
 });
+
+playGifForSpecifiedTime(5000, "fire16x16.gif");
+setTimeout(() => {
+    playGifForSpecifiedTime(5000, "test3.gif");
+}, 3000);
